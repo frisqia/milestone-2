f9d5779a95ae7550c6b30afd1d615c74e1bc8353
frisqia fatmalaningsih <frifani123@gmail.com>
2024-05-04 16:09:16 +0700

    dashboard

--- my-app/src/component/PokemonDashboard.tsx
+++ my-app/src/component/PokemonDashboard.tsx
@@ -1,50 +1,77 @@
-import React, { useEffect, useState } from "react";
+import { SetStateAction, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+interface PokemonDashboardProps {
+  isLoading: boolean;
+  setIsLoading: React.Dispatch<SetStateAction<boolean>>;
+}
 
 interface PokemonResult {
   name: string;
   url: string;
-  sprites: {
-    front_default: string;
-  };
+}
+
+interface PokemonResultWithImage {
+  name: string;
+  url: string;
+  imageUrl: string;
 }
 
 function PokemonDashboard() {
-  const [PokemonResults, setPokemoResults] = useState<PokemonResult[]>([]);
+  const [pokemonResults, setPokemonResults] = useState<
+    PokemonResultWithImage[]
+  >([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    async function fetchPokemon() {
+    async function fetchPokemons() {
       try {
         const url = "https://pokeapi.co/api/v2/pokemon";
         const response = await fetch(url);
+
         if (!response.ok) {
-          throw new Error("Failed Fetch pokemon data!");
+          throw new Error("Failed to fetch pokemon data!");
         }
         const data = await response.json();
-        //console.log(data.results, "INI DATA"); berhasil masuk object results
 
-        setPokemoResults(data.results);
+        const imageUrlArray = data.results.map((pokemon: PokemonResult) => ({
+          name: pokemon.name,
+          url: pokemon.url,
+          imageUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+            pokemon.url.split("/")[6]
+          }.png`,
+        }));
+
+        setPokemonResults(imageUrlArray);
         setIsLoading(false);
       } catch (error) {
         alert(error);
       }
     }
-    fetchPokemon();
+    fetchPokemons();
   }, []);
+
+  function handleNavigate(pokemon: PokemonResultWithImage) {
+    navigate(`/${pokemon.name}`, { state: pokemon });
+  }
+
   return (
-    <>
-      {isLoading == true && <h1>Loading ...</h1>}
-      {isLoading == false &&
-        PokemonResults.length > 0 &&
-        PokemonResults.map((result) => {
-          return (
-            <div>
-              <h1>{result.name}</h1>;
-              {/* <img src={result.sprites.front_default}></img> */}
-            </div>
-          );
-        })}
-    </>
+    <div className="grid grid-cols-5 gap-4">
+      {isLoading && <h1>Loading ....</h1>}
+      {!isLoading &&
+        pokemonResults.length > 0 &&
+        pokemonResults.map((result) => (
+          <div
+            key={result.name}
+            onClick={() => handleNavigate(result)}
+            className="rounded-lg bg-orange-500 shadow-md p-4 text-center relative"
+          >
+            <img src={result.imageUrl} alt={result.name} className="mx-auto" />
+            <h1 className="text-white font-bold text-lg mt-2">{result.name}</h1>
+          </div>
+        ))}
+    </div>
   );
 }
 
