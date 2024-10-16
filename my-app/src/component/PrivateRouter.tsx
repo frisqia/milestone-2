f24d835f435ef745db82268b2200d7e06117693b
Frisqia <frifani123@gmail.com>
2024-10-16 21:43:44 +0700

    readme documentasi revisi

--- my-app/src/component/PrivateRouter.tsx
+++ my-app/src/component/PrivateRouter.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 function PrivateRouter() {
   const isLogin = localStorage.getItem("token") !== null;
-  return isLogin ? <Outlet /> : <Navigate to="/Dashboard" />;
+  return isLogin ? <Outlet /> : <Navigate to="/Catalog" />;
 }
 export default PrivateRouter;
